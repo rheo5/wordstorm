6630f38fa1b1e7dad10b213c93ca45a22d5ded3e
rache111he <rachelheo455@gmail.com>
2024-03-03 15:34:25 -0800

    connect fend w bend

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
+import os
 from train import train_source 
 
 app = Flask(__name__)
 CORS(app)
 
-# Route to handle requests for similar words
+# Serve React static files
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path == "":
+        return send_from_directory('../frontend/build', 'index.html')
+    else:
+        if os.path.exists("../frontend/build/" + path):
+            return send_from_directory('../frontend/build', path)
+        else:
+            return send_from_directory('../frontend/build', 'index.html')
+
+# Your API route
 @app.route('/api/similar-words', methods=['POST'])
 def get_similar_words():
     # Get the input word from the request
@@ -14,8 +27,4 @@ def get_similar_words():
 
     similar_words = train_source(input_word, input_source)
 
-    # Return the list of similar words as JSON response
-    return jsonify(similarWords=similar_words)  
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    # Return the list of simila
